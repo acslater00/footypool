fb01b75fea4ac3f73f511f1bce7b15e047249b06
Adam Cohen <adam@seatgeek.com>
2014-06-16 13:58:41 -0400

    distribution query

--- footy/analysis.py
+++ footy/analysis.py
@@ -1,5 +1,6 @@
 import score
 from tables import Entrant, Game, Selection, EntrantSelection
+from sqlalchemy import *
 
 def rankings(session):
     entrants = session.query(Entrant).all()
@@ -10,4 +11,21 @@ def rankings(session):
     scored_entrants.sort(key=lambda t: t[1], reverse=True)
     return scored_entrants
 
+def print_rankings(session):
+    entrants = rankings(session)
+    for name, score in entrants:
+        print "{:30s}\t{} / 11 pts".format(name, score)
 
+
+def selection_distribution(session, selection_id):
+    ess = session.query(EntrantSelection.selection_value, func.count(1)).filter_by(selection_id=selection_id)
+    ks = ess.group_by(EntrantSelection.selection_value).order_by(desc(func.count(1)))
+    for pick, cnt in ks:
+        print "{:20s}\t{}".format(pick, cnt)
+
+def selection_distribution_multi(session, selection_ids):
+    ess = session.query(EntrantSelection.selection_value, func.count(1)).filter(
+        EntrantSelection.selection_id.in_(selection_ids))
+    ks = ess.group_by(EntrantSelection.selection_value).order_by(desc(func.count(1)))
+    for pick, cnt in ks:
+        print "{:20s}\t{}".format(pick, cnt)
