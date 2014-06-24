e4f43601aa23ccd24dad31f5aaca62ae1890ab60
Adam Cohen <adam@seatgeek.com>
2014-06-24 18:22:08 -0400

    print db creds in logs

--- footy/db.py
+++ footy/db.py
@@ -6,6 +6,8 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 if not DATABASE_URL:
     DATABASE_URL = os.environ.get('CLEARDB_DATABASE_URL')
 
+print 'DATABASE_URL=', DATABASE_URL
+
 def getSqlAlchemyConnection(connection_string, echo=False, pool_size=6):
     engine = create_engine(connection_string, echo=echo, pool_size=pool_size, pool_recycle=3600)
     maker = sessionmaker(bind=engine)
