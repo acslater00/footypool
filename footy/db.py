64ae21cd0f582edd278457dbafb01bb8d8073ba3
Adam Cohen <adam@seatgeek.com>
2014-06-24 18:25:09 -0400

    error in creating connections

--- footy/db.py
+++ footy/db.py
@@ -9,6 +9,8 @@ if not DATABASE_URL:
 print 'DATABASE_URL=', DATABASE_URL
 
 def getSqlAlchemyConnection(connection_string, echo=False, pool_size=6):
+    if "?reconnect=true" in connection_string:
+        connection_string = connection_string.replace("?reconnect=true", "?")
     engine = create_engine(connection_string, echo=echo, pool_size=pool_size, pool_recycle=3600)
     maker = sessionmaker(bind=engine)
     session = maker()
